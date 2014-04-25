0041f2afd427052878ca314ef7b45d158a7f8a13
jvoisin <julien.voisin@dustri.org>
2014-04-25 19:25:40 +0200

    Add a "comparison" page

---
+++ y/p/comparison.php
@@ -0,0 +1,118 @@
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Analysis</td>                  <td>radare2</td>    <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>Graph View</td>                <td>yes</td>        <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Signature recognition</td>     <td>yes</td>        <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Type support</td>              <td>yes</td>        <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>DWARF support</td>             <td>yes</td>        <td>yes</td> <td>no </td> </tr>
+<tr> <td width='50%'>Call/syscall recognation</td>  <td>yes</td>        <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Cross-references</td>          <td>yes</td>        <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>ROP-gadget finder</td>         <td>yes</td>        <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Architecture</td>              <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>ARM</td>                       <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>x86 and x64</td>               <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>CSR</td>                       <td>yes</td>     <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>TMS320C55x+</td>               <td>yes</td>     <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Bindings</td>                  <td>radare2</td>  <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>Javascript</td>                <td>yes</td>     <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>Python</td>                    <td>yes</td>     <td>yes</td> <td>yes </td> </tr>
+<tr> <td width='50%'>Ruby</td>                      <td>yes</td>     <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>Vala</td>                      <td>yes</td>     <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Debugger</td>                  <td>radare2</td>  <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>Remote debugging</td>          <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Breakpoints</td>               <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Tracing</td>                   <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Process attaching</td>         <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Decompilation</td>             <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>x86</td>                       <td>no </td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>x64</td>                       <td>no </td>     <td>no </td> <td>yes</td> </tr>
+<tr> <td width='50%'>ARM</td>                       <td>no </td>     <td>yes</td> <td>yes</td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Patching</td>                  <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>Writing/patching opcodes</td>  <td>yes</td>     <td>no </td> <td>yes</td> </tr>
+<tr> <td width='50%'>Shellcode compilation</td>     <td>yes</td>     <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>Patches generation</td>        <td>yes</td>     <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Platforms</td>                  <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>BSD</td>                       <td>yes</td>      <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>Linux</td>                     <td>yes</td>      <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>OSX</td>                       <td>yes</td>      <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Windows</td>                   <td>yes</td>      <td>yes</td> <td>yes</td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Prices and support</td>        <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>Free (as in freedom)</td>      <td>yes</td>     <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>Free (as in beer)</td>         <td>yes</td>     <td>no </td> <td>no </td> </tr>
+<tr> <td width='50%'>Professional customers support</td>    <td>no </td> <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Open bugtracker</td>           <td>yes</td>     <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>Project management</td>        <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>Saving and exporting</td>      <td>yes</td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>Collaborative work</td>        <td>wip</td>     <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
+
+<table class='comparison'>
+<thead>
+<tr> <td width='50%'>User interface</td>            <td>radare2</td> <td>IDA</td> <td>Hopper</td> </tr>
+</thead>
+<tbody>
+<tr> <td width='50%'>GUI</td>                       <td>no </td>     <td>yes</td> <td>yes</td> </tr>
+<tr> <td width='50%'>CLI</td>                       <td>yes</td>     <td>yes</td> <td>no </td> </tr>
+<tr> <td width='50%'>Themes support</td>            <td>yes</td>     <td>yes</td> <td>no </td> </tr>
+<tr> <td width='50%'>Web interface</td>             <td>wip</td>     <td>no </td> <td>no </td> </tr>
+</tbody>
+</table>
