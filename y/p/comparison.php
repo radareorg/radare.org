8414e022bdd2745d5787d735727df8d17465a27d
jvoisin <julien.voisin@dustri.org>
2014-06-14 17:24:27 +0200

    Add structures definition, and alphasort

--- y/p/comparison.php
+++ y/p/comparison.php
@@ -2,16 +2,17 @@ This is a (completely unfair) comparison between <a href='http://rada.re'>radare
 
 <table class='comparison'>
 <thead>
-<tr> <td width='50%'>Analysis</td>                  <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>Graph View</td>                <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'>Signature recognition</td>     <td class='y'>yes</td> <td class='y'><a href='https://www.hex-rays.com/products/ida/tech/flirt/in_depth.shtml'>yes</a></td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'>Type support</td>              <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'><a href='http://dwarfstd.org/'>DWARF</a> support</td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'><a href='http://lldb.llvm.org/symbols.html'>dSym</a> support</td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>Analysis</td>                  <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 <tr> <td width='50%'>Call/syscall recognition</td>  <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>Cross-references</td>          <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'><a href='http://dwarfstd.org/'>DWARF</a> support</td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'><a href='http://lldb.llvm.org/symbols.html'>dSym</a> support</td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>Graph View</td>                <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>ROP-gadget finder</td>         <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Signature recognition</td>     <td class='y'>yes</td> <td class='y'><a href='https://www.hex-rays.com/products/ida/tech/flirt/in_depth.shtml'>yes</a></td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>Custom structures definition</td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>no</td> </tr>
+<tr> <td width='50%'>Type support</td>              <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
@@ -19,11 +20,11 @@ This is a (completely unfair) comparison between <a href='http://rada.re'>radare
 <tr> <td width='50%'>Architecture</td>              <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
 <tr> <td width='50%'>ARM</td>                       <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'>Java/<a href='https://source.android.com/devices/tech/dalvik/index.html'>Dalvik</a></td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
-<tr> <td width='50%'>x86 and x64</td>               <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>CSR</td>                       <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
-<tr> <td width='50%'><a href='http://www.renesas.com/products/mpumcu/v850/index.jsp'>v850</a></td>                       <td class='y'>yes</td> <td class='n'>yes </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Java/<a href='https://source.android.com/devices/tech/dalvik/index.html'>Dalvik</a></td> <td class='y'>yes</td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
 <tr> <td width='50%'><a href='http://www.ti.com/lit/ug/spru393/spru393.pdf'>TMS320C55x+</a></td>               <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'><a href='http://www.renesas.com/products/mpumcu/v850/index.jsp'>v850</a></td>                       <td class='y'>yes</td> <td class='n'>yes </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>x86 and x64</td>               <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
@@ -40,28 +41,28 @@ This is a (completely unfair) comparison between <a href='http://rada.re'>radare
 <thead>
 <tr> <td width='50%'>Debugger</td>                  <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>Remote debugging</td>          <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>Breakpoints</td>               <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'>Tracing</td>                   <td class='y'>yes</td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
 <tr> <td width='50%'>Process attaching</td>         <td class='y'>yes</td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Remote debugging</td>          <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>Tracing</td>                   <td class='y'>yes</td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
 <thead>
 <tr> <td width='50%'>Decompilation</td>             <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>x86</td>                       <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'>x64</td>                       <td class='n'>no </td> <td class='n'>no </td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>ARM</td>                       <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>x64</td>                       <td class='n'>no </td> <td class='n'>no </td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>x86</td>                       <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
 <thead>
 <tr> <td width='50%'>Patching</td>                  <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>Writing/patching opcodes</td>  <td class='y'>yes</td> <td class='n'>no </td> <td class='y'>yes</td> </tr>
-<tr> <td width='50%'>Shellcode compilation</td>     <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
 <tr> <td width='50%'>Patches generation</td>        <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Shellcode compilation</td>     <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Writing/patching opcodes</td>  <td class='y'>yes</td> <td class='n'>no </td> <td class='y'>yes</td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
@@ -89,26 +90,26 @@ This is a (completely unfair) comparison between <a href='http://rada.re'>radare
 <thead>
 <tr> <td width='50%'>Prices and support</td>        <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>Free (as in freedom)</td>      <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
 <tr> <td width='50%'>Free (as in beer)</td>         <td class='y'>yes</td> <td class='n'>between 450&euro; and 2700&euro;</td> <td class='w'>between 65&euro; and 125&euro;</td> </tr>
-<tr> <td width='50%'>Professional customers support</td>    <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
+<tr> <td width='50%'>Free (as in freedom)</td>      <td class='y'>yes</td> <td class='n'>no </td> <td class='n'>no </td> </tr>
 <tr> <td width='50%'>Open bugtracker</td>           <td class='y'><a href='https://github.com/radare/radare2/issues'>yes</a></td> <td class='n'>no </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Professional customers support</td>    <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
 <thead>
 <tr> <td width='50%'>Project management</td>        <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>Saving and exporting</td>      <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>Collaborative work</td>        <td class='w'>wip</td> <td class='w'>Via plugins </td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>Saving and exporting</td>      <td class='y'>yes</td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 </tbody> </table>
 
 <table class='comparison'>
 <thead>
 <tr> <td width='50%'>User interface</td>            <td>radare2</td>       <td>IDA</td>           <td>Hopper</td> </tr>
 </thead> <tbody>
-<tr> <td width='50%'>GUI</td>                       <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>CLI</td>                       <td class='y'>yes</td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
+<tr> <td width='50%'>GUI</td>                       <td class='n'>no </td> <td class='y'>yes</td> <td class='y'>yes</td> </tr>
 <tr> <td width='50%'>Themes support</td>            <td class='y'><a href='https://github.com/radare/radare2/wiki/Colorschemes'>yes</a></td> <td class='y'>yes</td> <td class='n'>no </td> </tr>
 <tr> <td width='50%'>Web interface</td>             <td class='w'><a href='http://cloud.rada.re'>wip</a></td> <td class='n'>no </td> <td class='n'>no </td> </tr>
 </tbody> </table>
