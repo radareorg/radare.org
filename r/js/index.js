7f9e4ffd31d38b7dac6901635963602a23f109c8
pancake <pancake@nopcode.org>
2015-04-29 17:03:54 -0400

    Update android url

--- r/js/index.js
+++ r/js/index.js
@@ -6,7 +6,7 @@
       Win: { url: '/get/pkg/radare2-w32-0.9.9-git.zip', name: 'for Windows' },
       Mac: { url: '/get/pkg/radare2-0.9.9git.pkg', name: 'for OSX' },
       iPhone: { url: 'http://cydia.radare.org', name: 'Cydia' },
-      Android: { url: 'https://play.google.com/store/apps/details?id=org.radare.installer', name: 'Android' }
+      Android: { url: 'https://play.google.com/store/apps/details?id=org.radare2.installer', name: 'Android' }
     };
 
   document.addEventListener('DOMContentLoaded', function () {
