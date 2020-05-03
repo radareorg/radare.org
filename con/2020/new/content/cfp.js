f17d90a48a9bf950a4ef9003e4ebb1c8218d970f
murphy <dark.cancerbero@gmail.com>
2020-05-03 14:05:52 +0200

    add different crt styles

--- con/2020/new/content/cfp.js
+++ con/2020/new/content/cfp.js
@@ -1,5 +1,5 @@
 const title = "CFP";
-const content = "The Call for papers is also optimized for online: Login in <a href=\"https://con.rada.re/r2con-2020/\"> CFP Website </a> to submit your presentation!";
+const content = "The Call for papers is also optimized for online: Login in <a href=\"https://con.rada.re/r2con-2020/\">CFP Website</a> to submit your presentation!";
 const options = [
 				"Topics related to radare2 using radare2",	
 				"Talks can be 15/30/50 min long",
