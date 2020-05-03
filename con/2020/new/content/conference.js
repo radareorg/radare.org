c2a49761da074bc63797040a2ea16131bc18e8b5
murphy <dark.cancerbero@gmail.com>
2020-05-03 14:14:25 +0200

    Fix minor content issues

--- con/2020/new/content/conference.js
+++ con/2020/new/content/conference.js
@@ -3,7 +3,7 @@ const content = "\
 Thanks to the COVID19 global pandemic we have decided to make the conference purely online. </BR>\
 r2con is the annual conference about <a href=\"https://radare.org/\">radare2</a> \
 The conference aims to gather all the r2 users and developers around the world to enjoy a 4 day event full of experiments, hacking, teaching, discussing and coding stuff while having fun and drinking beers (safely at home).\
-Join the <a href=\"https://t.me/r2con2020\"> Telegram Channel </a> (broadcast) for further updates or refresh this page.";
+Join the <a href=\"https://t.me/r2con2020\">Telegram Channel</a> (broadcast) for further updates or refresh this page.";
 const options = [
 				"Days: 4 days (from 2020-09-02 to 2020-09-05)",	
 				"Schedule: 3-4h every day",
