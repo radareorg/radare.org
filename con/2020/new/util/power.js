acafaec698ec8cb578c3082de1705cb7522c651e
snxx-lppxx <snmgreenx@gmail.com>
2021-01-27 18:23:39 +0500

    Fixed icon path

--- con/2020/new/util/power.js
+++
@@ -1,8 +0,0 @@
-import { boot } from "./screens.js";
-
-/** Turn on the terminal */
-async function on() {
-	boot();
-}
-
-export { on };
--- con/2020/new/util/util/pause.js
+++
@@ -1,6 +0,0 @@
-/** Just a not so pretty way to make it easier to use setTimeout() with async/await */
-function pause(s = 1) {
-	return new Promise(resolve => setTimeout(resolve, 1000 * Number(s)));
-}
-
-export default pause;
